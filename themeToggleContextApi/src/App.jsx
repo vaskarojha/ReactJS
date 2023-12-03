22dae61e76a956c14747ac0ef4357883da1d8782
vaskarojha <vaskar.1125@gmail.com>
2023-12-03 19:31:24 +0100

    properly working toggle button

--- themeToggleContextApi/src/App.jsx
+++ themeToggleContextApi/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 import Toggle from './components/Toggle'
@@ -5,6 +6,22 @@ import Toggle from './components/Toggle'
 import { ThemeProvider } from './contexts/theme'
 
 function App() {
+
+  const [themeMode, setThemeMode] = useState('Light')
+
+  const darkTheme =() => {
+    setThemeMode('dark')
+  }
+
+  const lightTheme = () =>{
+    setThemeMode('light')
+  }
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove('dark', 'light')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
   return (
     <>
     <ThemeProvider value = {{themeMode, lightTheme, darkTheme}}>
