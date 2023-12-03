8c6249ad253cd6894ab94cfffdcbbbfc1435c3c4
vaskarojha <vaskar.1125@gmail.com>
2023-12-03 19:09:44 +0100

    component added and context created

--- themeToggleContextApi/src/App.jsx
+++ themeToggleContextApi/src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
+import Toggle from './components/Toggle'
+
+import { ThemeProvider } from './contexts/theme'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-<h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <ThemeProvider value = {{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                <Toggle/>
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+                <Card/>
+            </div>
+        </div>
+    </div>
+    </ThemeProvider>
     </>
   )
 }
--- themeToggleContextApi/src/components/Card.jsx
+++ themeToggleContextApi/src/components/Card.jsx
@@ -9,11 +9,11 @@ function Card(){
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Some random text over here.
+                        Some random text over here.Some random text over here.Some random text over here.
                     </h5>
                 </a>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">Hello World!!</span>
                     <a
                         href="/"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
