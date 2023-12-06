63f246242cda595b215cf645913739ec6324873a
vaskarojha <vaskar.1125@gmail.com>
2023-12-06 19:24:16 +0100

    useSelector and useDispatch

---
+++ reduxToolkitPractice/src/components/AddTodo.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import {useDispatch} from 'react-redux'
+import {addTodo} from '../features/todo/todoSlice'
+
+function AddTodo() {
+
+    const [input, setInput] = useState('')
+    const dispatch = useDispatch()
+
+    const addTodoHandler = (e)=>{
+        e.preventDefault()
+        dispatch(addTodo(input))
+        setInput('')
+    }
+
+  return (
+    <div>
+        <form onSubmit={addTodoHandler}>
+            <input type="text"
+            value={input}
+            onChange={(e)=>setInput(e.target.value)}
+            />
+            <button type='submit' />
+        </form>
+    </div>
+  )
+}
+
+export default AddTodo
---
+++ reduxToolkitPractice/src/components/Todos.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useSelector, useDispatch} from 'react-redux'
+import {removeTodo} from '../features/todo/todoSlice'
+
+function Todos() {
+    const todos = useSelector(state =>state.todos)
+  return (
+    <>
+    <div>Todos</div>
+    {todos.map((todo)=>(
+        <li key =  {todo.id}> {todo.text}<button onClick={()=>(
+            dispatch(removeTodo(todo.id))
+         )}>X</button></li>
+    )
+
+    )}
+</>
+  )
+  
+}
+
+export default Todos
