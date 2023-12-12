a78613e8a66508dec5f2f6b53e4556ec9cb59eb8
vaskarojha <vaskar.1125@gmail.com>
2023-12-12 20:06:11 +0100

    useRef Hook

--- reactHooks/useRefHook/src/App.jsx
+++ reactHooks/useRefHook/src/App.jsx
@@ -5,16 +5,25 @@ import './App.css'
 function App() {
   const [text,setText] = useState("")
   // const [renderCount, setRenderCount] = useState(0);
-  const renderCount = useRef(1)  
+  // const renderCount = useRef(1)  
+
+  const inputRef = useRef()
   
-  useEffect(()=>{
-    renderCount.currnet = renderCount.current +1
-  })
+  // useEffect(()=>{
+  //   renderCount.currnet = renderCount.current + 1
+  // })
+
+  const doSomething= ()=>{
+    console.log(inputRef.current)
+    inputRef.current.focus()
+  }
+
   return (
     <>
-      <input value={text}  onChange={e=>setText(e.target.value)} />
+      <input ref={inputRef} value={text}  onChange={e=>setText(e.target.value)} />
       <h2>You typed: {text}</h2>
-      <h3>Dom rendered {renderCount.current}</h3>
+      {/* <h3>Dom rendered: {renderCount.current} times!!</h3> */}
+      <button onClick={doSomething}>Type Now</button>
     </>
   )
 }
