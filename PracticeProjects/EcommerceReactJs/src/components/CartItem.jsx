915fea4369bf1f753460a401d487537c86ae0e36
vaskarojha <vaskar.1125@gmail.com>
2023-12-17 16:59:43 +0100

    cart item's operations

--- PracticeProjects/EcommerceReactJs/src/components/CartItem.jsx
+++ PracticeProjects/EcommerceReactJs/src/components/CartItem.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Link } from 'react-router-dom';
+import { CartContext } from '../contexts/CartContext';
+
+ 
+
 function CartItem({item}) {
-      // destructuring the item object that has been received
-  const {id, title, image, price, amount} = item;
+  const {removeFromCart, increaseAmount, decreaseAmount} = useContext(CartContext)
+  const {id, title,image,price,amount}= item;
   return (
+    <div className='flex gap-x-4 py-2 lg:px-4 border-b border-gray-200 w-full font-light text-gray-500'>
+      <div className='w-full min-h-[150px] flex items-center gap-x-4'>
+        <Link to = {`/product/${id}`}>
+          <img className='max-w-[80px]' src={image} alt="" />
+        </Link>
+        <div className='w-full flex flex-col'>
+          <div className='flex justify-between mb-2'>
+            <Link to={`/product/${id}` } className='text-sm uppercase font-medium max-w-[240px] text-primary hover:underline' >{title}</Link>
+            <div onClick={()=> removeFromCart(id)} className='text-xl cursor-pointer'> 
+              <span className='text-gray-500 hover:text-red-500 transition'>X</span> 
+              </div>
+          </div>
+          <div className=' flex gap-x-2 h-[36px] text-sm'>
+            <div className='flex felx-1 max-w-[100px] items-center h-full border text-primary font-medium'>
+              <div onClick={()=> decreaseAmount(id)} className='flex-1 h-1 full flex justify-center items-center cursor-pointer'><span>-</span></div>
+              
+            <div className='h-full flex justify-center items-center px-2'>{amount}</div>
+            <div onClick={()=> increaseAmount(id)} className='flex-1 h-1 full flex justify-center items-center cursor-pointer' ><span>+</span></div>
+            </div>
 
-    <div className='flex'>
-      <div className='w-full min-h-[150px] flex items-center gap-x-5'>
-    
-        <div >
-          <Link to={`/product/${id}`}>
-          <img className="max-w-[80px]" src={image} alt="" />
-          </Link>
+            <div className='flex-1 flex items-center justify-around'>$ {price}</div>
+            <div className='flex-1 flex justify-end items-center text-primary font-medium'>{`$ ${parseFloat(price * amount).toFixed(2)}`}</div>
+          </div>
         </div>
-        <div className='flex justify-between mb-2 '>
-        <Link to={`/product/${id}`} className='text-sm uppercase font-medium max-w-[240px] text-primary hover:underline'>{title}</Link>
-        </div>
-        <div className='bg-blue-300 flex gap-x-2 h-[36px]'>
-
-        </div>
-        <div className='cursor-pointer '>
-          <span className='text-xl text-red-500'>REMOVE</span>
-        </div>
-        
       </div>
     </div>
   )
--- PracticeProjects/EcommerceReactJs/src/components/Sidebar.jsx
+++ PracticeProjects/EcommerceReactJs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import CartItem from './CartItem'
 import { SidebarContext } from '../contexts/SidebarContex'
 
@@ -6,7 +7,7 @@ import { SidebarContext } from '../contexts/SidebarContex'
 import { CartContext } from '../contexts/CartContext'
 function Sidebar() {
   const {isOpen,handleClose } = useContext(SidebarContext);
-  const {cart} = useContext(CartContext)
+  const {cart, clearCart} = useContext(CartContext)
 
   return (
     <div className={`${isOpen?'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
@@ -18,10 +19,18 @@ function Sidebar() {
         </div>
       </div>
       <div>
-        {}
         {cart.map((item)=>{
           return <CartItem item = {item} key={item.it}/>
         })}
+      </div>
+
+      <div className=' flex flex-col gap-y-3 py-4 mt-4'>
+        <div className='flex w-full justify-between items-center'>
+          <div className='uppercase font-semibold'>
+            <span className='mr-2'>Total: </span>
+          </div>
+          <div onClick={clearCart} className='cursor-pointer text-red-500 py-4 text-lg flex justify-center items-center text-xl'>Clear cart</div>
+        </div>
       </div>
     </div>
     
