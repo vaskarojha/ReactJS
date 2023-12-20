d19e31897d4f7e1bdabb521e3429ca1dac3cc610
vaskarojha <vaskar.1125@gmail.com>
2023-12-20 16:59:21 +0100

    displaying components in app

--- PracticeProjects/blogWithAppwrite/src/App.jsx
+++ PracticeProjects/blogWithAppwrite/src/App.jsx
@@ -1,14 +1,40 @@
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
 import './App.css'
+import {} from "./store/authSlice"
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((data)=>{
+      if(data){
+        dispatch(login({data:userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  }, [])
 
-<>
-Blog app with authentication.
-{console.log(import.meta.env.VITE_APPWRITE_URL)}
-</>
-  )
+  return !loading? (
+    <div className='min-h-screen flex flex-wrap content-between bg-blue-300'>
+      <div className='w-full block'>
+        <Header/>
+          <main>
+            {/* <Outlet/> */}
+          </main>
+        <Footer/>
+      </div>
+    </div>
+  ): null
 }
 
 export default App
