26a5e596a85d187522199e85d70608394b84f615
vaskarojha <vaskar.1125@gmail.com>
2023-12-30 16:13:19 +0100

    All Post and edit Post

--- PracticeProjects/blogWithAppwrite/src/components/index.js
+++ PracticeProjects/blogWithAppwrite/src/components/index.js
@@ -2,9 +2,20 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Container from "postcss/lib/container";
 import LogoutButton from "./LogoutButton";
+import Login from "../Login";
+import Signup from "../Signup";
+import Post from "./Post";
+import PostForm from "./post-form/PostForm";
+
 export {
     Header,
     Footer,
     LogoutButton,
     Container,
+    Login,
+    RTE,
+    Select,
+    Signup,
+    Post,
+    PostForm
 }
---
+++ PracticeProjects/blogWithAppwrite/src/pages/EditPost.jsx
@@ -0,0 +1,34 @@
+import React,{useEffect, useState} from 'react'
+import {Container, PostForm} from '../components'
+import appwriteService from '../appwrite/appwriteConfig'
+import { useNavigate, useParams } from 'react-router-dom'
+
+function EditPost() {
+    const [posts, setPosts] = useState(null)
+    const {slug} = useParams()
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(slug){
+            appwriteService.getPost(slug).then((post)=>{
+                if(post){
+                    setPosts(post)
+                }
+            })
+        }else{
+            navigate('/')
+        }
+    }, [slug, navigate])
+    
+  return (
+    post ? (
+        <div className='py-8'>
+            <Container>
+            <PostForm post = {post}/>
+            </Container>
+        </div>
+    ): null
+  )
+}
+
+export default EditPost
