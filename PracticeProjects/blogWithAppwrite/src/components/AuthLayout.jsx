6d7c1608a6786f60b1bd4c2188e4dbf088e3e2ef
vaskarojha <vaskar.1125@gmail.com>
2023-12-30 09:23:42 +0100

    Add real time editor

--- PracticeProjects/blogWithAppwrite/src/components/AuthLayout.jsx
+++ PracticeProjects/blogWithAppwrite/src/components/AuthLayout.jsx
@@ -14,7 +14,7 @@ export default function Protected({children, authentication = true}) {
     const authStatus = useSelector(state => state.auth.status)
     
     useEffect(()=>{
-        true && true
+        // true && true
         if(authentication && authStatus !== authentication){
             navigate("/login")
         } else if(!authentication && authStatus !== authentication){
